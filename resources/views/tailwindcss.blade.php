1b41d70c85a7738baeb4a6aeaa4b0eb20c63af6b
EtudiantFabre <fabricetoyi87@gmail.com>
2023-09-07 19:01:46 +0000

    Ajout de tailwindcss dans le projet laravel

---
+++ resources/views/tailwindcss.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body class="container mx-auto">
+    <div class="m-auto">
+        <div class="flex bg-slate-100 flex-col">
+            <h1 class="text-3xl font-bold  text-center">
+                Hello world!
+            </h1>
+            <button class="rounded-full bg-slate-700 p-4 mt-4 hover:text-lime-50">
+                Test pour le tailwindcss
+            </button>
+        </div>
+    </div>
+
+</body>
+
+</html>
--- webpack.mix.js
+++
@@ -1,4 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass("resources/scss/app.scss", "public/css");
